1a0de6ce0ab5781b73c2705522b1128674608387
vm799 <89162649+vm799@users.noreply.github.com>
2021-11-27 12:20:31 +0000

    react component files set up

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <div>
+        <header>
+        <nav>
+        <h2>meme generator</h2>
+        </nav></header>
+        </div>
+    )
+}
